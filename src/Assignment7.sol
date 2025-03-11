eefaafb925ecd7b9f16a4f2855525af181105654
Amgad <amgagmohamed38@gmail.com>
2025-03-12 01:36:34 +0200

    Assignment 7

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721PausableUpgradeable.sol";
 contract Assignment7 is ERC20 {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    constructor( string memory name, string memory symbol) ERC20( name, symbol) {}
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
+    function mint(address to, uint256 amount) external {
         // Fill in the logic
         // call _mint function with to and amount params
+        _mint(to, amount);
     }
 }
